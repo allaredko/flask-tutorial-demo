c6d8061a2e50f8ba81ae5801fd7882d9e62a8a40
alla redko <alla.redko@jetbrains.com>
2018-11-01 17:42:30 +0300

    Password

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def login(city_id):
         if request.method == "POST":
             attempted_username = request.form['username']
             attempted_password = request.form['password']
-            if attempted_username == 'admin' and attempted_password == 'password':
+            if attempted_username == 'admin' and attempted_password == os.environ['USER_PASSWORD']:
                 session['logged_in'] = True
                 session['username'] = request.form['username']
                 return redirect(url_for('edit_database', city_id=city_id))
